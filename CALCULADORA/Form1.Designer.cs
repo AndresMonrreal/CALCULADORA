f91f0b03d9fed7f480c20764773163f54136f2c8
andres <andresmonrreal4@gmail.com>
2026-01-30 15:50:35 -0600

    Se agrego el formulario del mi calculadora y se empezo a implementar la logica de cada boton

--- CALCULADORA/Form1.Designer.cs
+++ CALCULADORA/Form1.Designer.cs
@@ -28,12 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            txtTexto = new TextBox();
+            but1 = new Button();
+            but2 = new Button();
+            but3 = new Button();
+            but4 = new Button();
+            but5 = new Button();
+            but6 = new Button();
+            but8 = new Button();
+            but7 = new Button();
+            but9 = new Button();
+            but0 = new Button();
+            butPunto = new Button();
+            butResta = new Button();
+            butSuma = new Button();
+            butMultiplicar = new Button();
+            butDIvision = new Button();
+            butResto = new Button();
+            butDe = new Button();
+            butEliminar = new Button();
+            SuspendLayout();
+            // 
+            // txtTexto
+            // 
+            txtTexto.Location = new Point(27, 79);
+            txtTexto.Name = "txtTexto";
+            txtTexto.Size = new Size(307, 27);
+            txtTexto.TabIndex = 0;
+            txtTexto.TextChanged += txtTexto_TextChanged;
+            // 
+            // but1
+            // 
+            but1.Location = new Point(40, 255);
+            but1.Name = "but1";
+            but1.Size = new Size(94, 29);
+            but1.TabIndex = 1;
+            but1.Text = "1";
+            but1.UseVisualStyleBackColor = true;
+            but1.Click += button1_Click;
+            // 
+            // but2
+            // 
+            but2.Location = new Point(140, 255);
+            but2.Name = "but2";
+            but2.Size = new Size(94, 29);
+            but2.TabIndex = 2;
+            but2.Text = "2";
+            but2.UseVisualStyleBackColor = true;
+            but2.Click += but2_Click;
+            // 
+            // but3
+            // 
+            but3.Location = new Point(240, 255);
+            but3.Name = "but3";
+            but3.Size = new Size(94, 29);
+            but3.TabIndex = 3;
+            but3.Text = "3";
+            but3.UseVisualStyleBackColor = true;
+            but3.Click += but3_Click;
+            // 
+            // but4
+            // 
+            but4.Location = new Point(40, 309);
+            but4.Name = "but4";
+            but4.Size = new Size(94, 29);
+            but4.TabIndex = 4;
+            but4.Text = "4";
+            but4.UseVisualStyleBackColor = true;
+            but4.Click += button4_Click;
+            // 
+            // but5
+            // 
+            but5.Location = new Point(140, 309);
+            but5.Name = "but5";
+            but5.Size = new Size(94, 29);
+            but5.TabIndex = 5;
+            but5.Text = "5";
+            but5.UseVisualStyleBackColor = true;
+            but5.Click += but5_Click;
+            // 
+            // but6
+            // 
+            but6.Location = new Point(240, 309);
+            but6.Name = "but6";
+            but6.Size = new Size(94, 29);
+            but6.TabIndex = 6;
+            but6.Text = "6";
+            but6.UseVisualStyleBackColor = true;
+            but6.Click += button6_Click;
+            // 
+            // but8
+            // 
+            but8.Location = new Point(140, 357);
+            but8.Name = "but8";
+            but8.Size = new Size(94, 29);
+            but8.TabIndex = 7;
+            but8.Text = "8";
+            but8.UseVisualStyleBackColor = true;
+            but8.Click += but8_Click;
+            // 
+            // but7
+            // 
+            but7.Location = new Point(40, 357);
+            but7.Name = "but7";
+            but7.Size = new Size(94, 29);
+            but7.TabIndex = 8;
+            but7.Text = "7";
+            but7.UseVisualStyleBackColor = true;
+            but7.Click += but7_Click;
+            // 
+            // but9
+            // 
+            but9.Location = new Point(240, 357);
+            but9.Name = "but9";
+            but9.Size = new Size(94, 29);
+            but9.TabIndex = 9;
+            but9.Text = "9";
+            but9.UseVisualStyleBackColor = true;
+            but9.Click += but9_Click;
+            // 
+            // but0
+            // 
+            but0.Location = new Point(140, 408);
+            but0.Name = "but0";
+            but0.Size = new Size(94, 29);
+            but0.TabIndex = 10;
+            but0.Text = "0";
+            but0.UseVisualStyleBackColor = true;
+            but0.Click += button10_Click;
+            // 
+            // butPunto
+            // 
+            butPunto.Location = new Point(240, 408);
+            butPunto.Name = "butPunto";
+            butPunto.Size = new Size(94, 29);
+            butPunto.TabIndex = 11;
+            butPunto.Text = ".";
+            butPunto.UseVisualStyleBackColor = true;
+            butPunto.Click += butPunto_Click;
+            // 
+            // butResta
+            // 
+            butResta.Location = new Point(140, 148);
+            butResta.Name = "butResta";
+            butResta.Size = new Size(94, 29);
+            butResta.TabIndex = 12;
+            butResta.Text = "-";
+            butResta.UseVisualStyleBackColor = true;
+            butResta.Click += butResta_Click;
+            // 
+            // butSuma
+            // 
+            butSuma.Location = new Point(40, 148);
+            butSuma.Name = "butSuma";
+            butSuma.Size = new Size(94, 29);
+            butSuma.TabIndex = 13;
+            butSuma.Text = "+";
+            butSuma.UseVisualStyleBackColor = true;
+            // 
+            // butMultiplicar
+            // 
+            butMultiplicar.Location = new Point(240, 148);
+            butMultiplicar.Name = "butMultiplicar";
+            butMultiplicar.Size = new Size(94, 29);
+            butMultiplicar.TabIndex = 14;
+            butMultiplicar.Text = "*";
+            butMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // butDIvision
+            // 
+            butDIvision.Location = new Point(40, 202);
+            butDIvision.Name = "butDIvision";
+            butDIvision.Size = new Size(94, 29);
+            butDIvision.TabIndex = 15;
+            butDIvision.Text = "/";
+            butDIvision.UseVisualStyleBackColor = true;
+            // 
+            // butResto
+            // 
+            butResto.Location = new Point(140, 202);
+            butResto.Name = "butResto";
+            butResto.Size = new Size(94, 29);
+            butResto.TabIndex = 16;
+            butResto.Text = "%";
+            butResto.UseVisualStyleBackColor = true;
+            // 
+            // butDe
+            // 
+            butDe.Location = new Point(240, 202);
+            butDe.Name = "butDe";
+            butDe.Size = new Size(94, 29);
+            butDe.TabIndex = 17;
+            butDe.Text = "De";
+            butDe.UseVisualStyleBackColor = true;
+            butDe.Click += button4_Click_1;
+            // 
+            // butEliminar
+            // 
+            butEliminar.Location = new Point(40, 408);
+            butEliminar.Name = "butEliminar";
+            butEliminar.Size = new Size(94, 29);
+            butEliminar.TabIndex = 19;
+            butEliminar.Text = "Elim";
+            butEliminar.UseVisualStyleBackColor = true;
+            butEliminar.Click += butEliminar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(404, 511);
+            Controls.Add(butEliminar);
+            Controls.Add(butDe);
+            Controls.Add(butResto);
+            Controls.Add(butDIvision);
+            Controls.Add(butMultiplicar);
+            Controls.Add(butSuma);
+            Controls.Add(butResta);
+            Controls.Add(butPunto);
+            Controls.Add(but0);
+            Controls.Add(but9);
+            Controls.Add(but7);
+            Controls.Add(but8);
+            Controls.Add(but6);
+            Controls.Add(but5);
+            Controls.Add(but4);
+            Controls.Add(but3);
+            Controls.Add(but2);
+            Controls.Add(but1);
+            Controls.Add(txtTexto);
+            Name = "Form1";
+            Text = "Calculadora";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtTexto;
+        private Button but1;
+        private Button but2;
+        private Button but3;
+        private Button but4;
+        private Button but5;
+        private Button but6;
+        private Button but8;
+        private Button but7;
+        private Button but9;
+        private Button but0;
+        private Button butPunto;
+        private Button butResta;
+        private Button butSuma;
+        private Button butMultiplicar;
+        private Button butDIvision;
+        private Button butResto;
+        private Button butDe;
+        private Button butEliminar;
     }
 }
--- CALCULADORA/Form1.cs
+++ CALCULADORA/Form1.cs
@@ -6,5 +6,102 @@ namespace CALCULADORA
         {
             InitializeComponent();
         }
+        double a = 0;
+        double b = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtTexto.Text = "0";
+        }
+
+        private void txtTexto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "o") txtTexto.Text = "";
+            txtTexto.Text += "1";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "4";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "6") txtTexto.Text = "";
+            txtTexto.Text += "6";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "0";
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (txtTexto.TextLength == 1) txtTexto.Text = "o";
+            else (txtTexto.TextLength != 1) t
+        }
+
+        private void butResta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "2";
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "3";
+        }
+
+        private void but5_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "5";
+        }
+
+        private void but7_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "7";
+        }
+
+        private void but8_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "8";
+        }
+
+        private void but9_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == "0") txtTexto.Text = "";
+            txtTexto.Text += "9";
+        }
+
+        private void butPunto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butEliminar_Click(object sender, EventArgs e)
+        {
+            txtTexto.Text = " 0";
+            a = 0;
+            b = 0;
+        }
     }
 }
